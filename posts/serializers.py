370a408226520dce99c7bbe14030f80e852c5176
Neillcllghn <>
2023-06-25 15:49:59 +0000

    added post function and IsAuthenticatedOrReadOnly

--- posts/serializers.py
+++ posts/serializers.py
@@ -31,6 +31,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'image_filter',
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter',
         ]
