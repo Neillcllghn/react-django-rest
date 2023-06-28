a2fcf1366d6969137f5fb8303484eee67309efad
Neillcllghn <>
2023-06-28 19:06:05 +0000

    adding following_id and like_id fields to profile and post

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Likes
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
 
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
@@ -28,10 +30,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Likes.objects.filter(
+                owner=user, post=obj
+            ).first()
+            print(like)
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter',
+            'title', 'content', 'image', 'image_filter', 'like_id',
         ]
