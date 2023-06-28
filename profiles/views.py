66c9034e533cbdb2c34debcb9310e8cd5355be21
Neillcllghn <>
2023-06-28 19:38:29 +0000

    Adding post_count, followers_count and following_count to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
@@ -16,7 +17,21 @@ No Create view (post method), as profile creation handled by django signals
 class ProfileList(APIView):
 
     def get(self, request):
-        profiles = Profile.objects.all()
+        profiles = Profile.objects.annotate(
+            posts_count=Count('owner__post', distinct=True),
+            followers_count=Count('owner__followed', distinct=True),
+            following_count=Count('owner__following', distinct=True)
+        ).order_by('-created_at')
+        # filter_backends = [
+        #     filters.OrderingFilter
+        # ]
+        ordering_fields = [
+            'posts_count',
+            'followers_count',
+            'following_count',
+            'owner__following__created_at',
+            'owner__followed__created_at',
+        ]
         serializer = ProfileSerializer(
             profiles, many=True, context={'request': request}
         )
