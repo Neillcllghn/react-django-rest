33ec83f975790abe4e7bc3c1bceb3ef9c59bf65e
Neillcllghn <>
2023-06-30 16:43:03 +0000

    adding search field to filter in Posts

--- posts/views.py
+++ posts/views.py
@@ -1,32 +1,80 @@
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(APIView):
+class PostList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = PostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
     ]
 
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(
-            posts, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = PostSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-                )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+
+
+# from rest_framework import status, permissions
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from .models import Post
+# from .serializers import PostSerializer
+
+
+# class PostList(APIView):
+#     serializer_class = PostSerializer
+#     permission_classes = [
+#         permissions.IsAuthenticatedOrReadOnly
+#     ]
+
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(
+#             posts, many=True, context={'request': request}
+#         )
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = PostSerializer(
+#             data=request.data, context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save(owner=request.user)
+#             return Response(
+#                 serializer.data, status=status.HTTP_201_CREATED
+#                 )
+#         return Response(
+#             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+#         )
