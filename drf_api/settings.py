9cbc7e8ca63205430cb0cef58f33adf8d9953be4
Neillcllghn <>
2023-07-03 16:39:11 +0000

    api updateds

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -63,8 +64,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 # ALLOWED_HOSTS = ['8000-neillcllghn-react-django-1ey1aziz1e.us2.codeanyapp.com']
-ALLOWED_HOSTS = ['8000-neillcllghn-react-django-1ey1aziz1e.us2.codeanyapp.com', 
-                 'localhost', 'djangorestframework-project1.herokuapp.com']
+ALLOWED_HOSTS = ['8000-neillcllghn-react-django-1ey1aziz1e.us2.codeanyapp.com',
+                 os.environ.get('ALLOWED_HOST'), 'localhost',]
 
 
 # Application definition
@@ -107,14 +108,23 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
-else:
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^([^.]+)', os.environ.get(
+        'CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+
     CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.gitpod\.io$",
-     ]
+        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
+    ]
+
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN')
+#     ]
+# else:
+#     CORS_ALLOWED_ORIGIN_REGEXES = [
+#         r"^https://.*\.gitpod\.io$",
+#     ]
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -144,15 +154,15 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 
 if 'DEV' in os.environ:
     DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
-         }
-     }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 else:
     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-     }
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
